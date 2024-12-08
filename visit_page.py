64e3ae43b25a54ce7a2926679d54b3b13350e8fe
KhoLiup <39913931+KhoLiup@users.noreply.github.com>
2024-12-08 08:31:34 +0400

    Update visit_page.py

--- visit_page.py
+++ visit_page.py
@@ -1,13 +1,29 @@
 import requests
 
-def visit_site():
-    url = "http://wp.pavilion.az/send_reminder.php"
-    try:
-        response = requests.get(url)
-        print(f"Visited {url} - Status Code: {response.status_code}")
-        print(f"Response Content: {response.text[:100]}...")  # İlk 100 simvolu göstərir
-    except Exception as e:
-        print(f"Error visiting site: {e}")
+class SiteVisitor:
+    def __init__(self, url):
+        self.url = url
+
+    def visit(self):
+        try:
+            response = requests.get(self.url)
+            self._log_response(response)
+        except requests.exceptions.RequestException as e:
+            self._log_error(e)
+
+    def _log_response(self, response):
+        print(f"Visited {self.url}")
+        print(f"Status Code: {response.status_code}")
+        if response.ok:
+            print(f"Response Content: {response.text[:100]}...")  # İlk 100 simvolu göstərir
+        else:
+            print("Failed to retrieve the content.")
+
+    def _log_error(self, error):
+        print(f"Error visiting site: {error}")
+
 
 if __name__ == "__main__":
-    visit_site()
+    url = "https://wp.pavilion.az/send_reminder.php"
+    visitor = SiteVisitor(url)
+    visitor.visit()
