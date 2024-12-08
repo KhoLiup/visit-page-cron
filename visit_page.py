2466ca2c13f0f125b46ffebe6ddf1c7fd0bb1910
KhoLiup <39913931+KhoLiup@users.noreply.github.com>
2024-12-08 08:20:11 +0400

    Update visit_page.py

--- visit_page.py
+++ visit_page.py
@@ -1,9 +1,13 @@
 import requests
 
-# Saytı ziyarət edən funksiya
 def visit_site():
-    url = "http://wp.pavilion.az/send_reminder.php"  # Ziyarət ediləcək saytın URL-si
-    response = requests.get(url)  # Sayta GET sorğusu göndər
+    url = "http://wp.pavilion.az/send_reminder.php"
+    try:
+        response = requests.get(url)
+        print(f"Visited {url} - Status Code: {response.status_code}")
+        print(f"Response Content: {response.text[:100]}...")  # İlk 100 simvolu göstərir
+    except Exception as e:
+        print(f"Error visiting site: {e}")
 
-# Saytı ziyarət et
-visit_site()
+if __name__ == "__main__":
+    visit_site()
